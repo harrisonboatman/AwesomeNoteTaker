1026daf175558dfe50046eb96c2f7d91554a4934
Harrison Boatman <hrry.boatman2@gmail.com>
2023-02-01 10:03:42 -0600

    updated PORT variable

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { readFromFile, readAndAppend } = require('./helpers/fsUtils')
 const uuid = require ('./helpers/uuid')
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
